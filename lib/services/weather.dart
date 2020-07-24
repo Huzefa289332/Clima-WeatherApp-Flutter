4d71649ac4486b25786e5049f26fb1d90be5d3a3
Huzefa289332 <huzaifa.hb.123@gmail.com>
2020-07-24 19:25:41 +0500

    Complete

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,29 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '47641098ebc5e084eb2f0b49d7cd795b';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+//lat=24.8542989985&lon=67.0116845978
+//lat=${location.latitude}&lon=${location.longitude}
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper =
+        NetworkHelper('$openWeatherMapURL?q=$cityName&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
